f0b756b472593817a7e63c473ba242920a2b6466
Jacob Danell <jacob@emberlight.se>
2023-08-20 23:14:00 +0200

    Added fusion version to info

--- .fusion_typings/__builtins__.pyi
+++ .fusion_typings/__builtins__.pyi
@@ -3,6 +3,8 @@ v1.1
 Generated with FMD Fusion Scriptsing Stubs
 https://github.com/EmberLightVFX/BMD-Fusion-Scripting-Stubs
 
+Generated with Fusion Studio 18.5 build 73
+
 """
 
 from _tool_scripts import fusion, fu, composition, comp, tool
