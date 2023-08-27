2d4a35201fddc3cd2fd1327cc582586b25611549
Jacob Danell <jacob@emberlight.se>
2023-08-27 18:07:26 +0200

    Moved default-generations to its own file

---
+++ manual_fixes/generate_default_files.py
@@ -0,0 +1,56 @@
+def generateToolClass() -> str:
+    return """\
+from Operator import Operator_
+
+Tool_ = Operator_
+
+"""
+
+
+def generateToolScripts() -> str:
+    return """\
+from Fusion import Fusion_
+from Composition import Composition_
+from Tool import Tool_
+
+fusion = Fusion_()
+fu = Fusion_()
+
+composition = Composition_()
+comp = Composition_()
+
+tool = Tool_()
+
+"""
+
+
+def generateBuiltins() -> str:
+    return '''\
+"""
+v1.1
+Generated with FMD Fusion Scriptsing Stubs
+https://github.com/EmberLightVFX/BMD-Fusion-Scripting-Stubs
+
+Generated with Fusion Studio 18.5 build 73
+
+"""
+
+# Fusion objects
+from _tool_scripts import (
+    fusion,
+    fu,
+    composition,
+    comp,
+    tool,
+)
+
+__all__ = [
+    "fusion",
+    "fu",
+    "composition",
+    "comp",
+    "tool"
+]
+
+# Stubs objects
+'''
--- stubs_generator.py
+++ stubs_generator.py
@@ -29,6 +29,11 @@ from manual_fixes import (
     fixInputTypes,
     fixPropertyReturnTypes,
 )
+from manual_fixes.generate_default_files import (
+    generateToolClass,
+    generateToolScripts,
+    generateBuiltins,
+)
 
 
 return_types: list[str] = []
@@ -96,64 +101,6 @@ def generateNonExistingClasses(non_existing_types: list[str]) -> str:
         content += f"{obj} = {obj}_\n"
     content += "\n"
     return content
-
-
-def generateToolClass() -> str:
-    return """\
-from Operator import Operator_
-
-Tool_ = Operator_
-
-"""
-
-
-def generateToolScripts() -> str:
-    return """\
-from Fusion import Fusion_
-from Composition import Composition_
-from Tool import Tool_
-
-fusion = Fusion_()
-fu = Fusion_()
-
-composition = Composition_()
-comp = Composition_()
-
-tool = Tool_()
-
-"""
-
-
-def generateBuiltins() -> str:
-    return '''\
-"""
-v1.1
-Generated with FMD Fusion Scriptsing Stubs
-https://github.com/EmberLightVFX/BMD-Fusion-Scripting-Stubs
-
-Generated with Fusion Studio 18.5 build 73
-
-"""
-
-# Fusion objects
-from _tool_scripts import (
-    fusion,
-    fu,
-    composition,
-    comp,
-    tool,
-)
-
-__all__ = [
-    "fusion",
-    "fu",
-    "composition",
-    "comp",
-    "tool"
-]
-
-# Stubs objects
-'''
 
 
 def typeConverter(
