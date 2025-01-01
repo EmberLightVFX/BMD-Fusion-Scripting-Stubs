8c3e18f2833870e53e8b2244548f0ae1a23b4404
Ember Light <jacob@emberlight.se>
2025-01-01 21:03:07 +0100

    Change version to 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="fusion-stubs",
-    version="1.0.0",
+    version="1.5.0",
     description="Python stubs for BMD Fusion scripting",
     author="Jacob Danell @ Ember Light",
     author_email="jacob@emberlight.se",
