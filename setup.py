22cfc1ba8803fdb713634afe4ff1bbdf7ce23d13
Ember Light <jacob@emberlight.se>
2025-01-01 19:52:40 +0100

    Package setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="fusion-stubs",
+    version="1.0.0",
+    description="Python stubs for BMD Fusion scripting",
+    author="Jacob Danell @ Ember Light",
+    author_email="jacob@emberlight.se",
+    url="https://github.com/EmberLightVFX/BMD-Fusion-Scripting-Stubs",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Typing :: Stubs Only",
+    ],
+    packages=find_packages(include=["fusion-stubs", "fusion-stubs.*"]),
+    package_data={
+        "fusion-stubs": ["*.pyi", "**/*.pyi", "py.typed"],
+    },
+    zip_safe=False,
+)
